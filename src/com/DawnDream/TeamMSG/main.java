82296f34d0abc161f6e74b50b0ceda8dce5d76d7
Dawn-Dream <1040545078@qq.com>
2020-11-14 15:34:55 +0800

    first commit

---
+++ src/com/DawnDream/TeamMSG/main.java
@@ -0,0 +1,11 @@
+package com.DawnDream.TeamMSG;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+    @Override
+    public void onLoad(){
+        System.out.println(ChatColor.RED + "TeamMSG Plugin is Loading");
+    }
+}
